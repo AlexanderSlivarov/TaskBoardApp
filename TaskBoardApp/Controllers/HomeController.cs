6fc6cfc90855c04e775f2b43a7c78bf367a3eccd
Alexander <metalshard05@gmail.com>
2024-02-12 09:32:36 +0200

    The applications is ready!

--- TaskBoardApp/Controllers/HomeController.cs
+++ TaskBoardApp/Controllers/HomeController.cs
@@ -1,14 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
+using TaskBoardApp.Data;
 using TaskBoardApp.Models;
 
 namespace TaskBoardApp.Controllers
 {
     public class HomeController : Controller
-    {       
+    {
+        private readonly TaskBoardAppDbContext _data;
+
+        public HomeController(TaskBoardAppDbContext context)
+        {
+            _data = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var taskBoards = _data
+                .Boards
+                .Select(b => b.Name)
+                .Distinct();
+
+            var tasksCounts = new List<HomeBoardModel>();
+            foreach (var boardName in taskBoards)
+            {
+                var tasksInBoard = _data.Tasks.Where(t => t.Board.Name == boardName).Count();
+                tasksCounts.Add(new HomeBoardModel()
+                {
+                    BoardName = boardName,
+                    TasksCount = tasksInBoard
+                });
+            }
+
+            var usertasksCount = -1;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                usertasksCount = _data.Tasks.Where(t => t.OwnerId == currentUserId).Count();
+            }
+
+            var homeModel = new HomeViewModel()
+            {
+                AllTasksCount = _data.Tasks.Count(),
+                BoardsWithTasksCount = tasksCounts,
+                UserTasksCount = usertasksCount
+            };
+
+            return View(homeModel);
         }               
     }
 }
--- TaskBoardApp/Models/Task/TaskFormModel.cs
+++ TaskBoardApp/Models/Task/TaskFormModel.cs
@@ -1,10 +1,34 @@
 ﻿using Microsoft.Build.Framework;
 using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
 namespace TaskBoardApp.Models.Task
 {
     public class TaskFormModel
     {
-       
+        public const int TaskMaxTitle = 70;
+        public const int TaskMinTitle = 5;
+
+        public const int TaskMaxDescription = 1000;
+        public const int TaskMinDescription = 10;
+
+        [Required]
+        [StringLength(TaskMaxTitle, MinimumLength = TaskMinTitle, ErrorMessage = "Title should be at least {2} characters long.")]
+
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(TaskMaxDescription, MinimumLength = TaskMinDescription, ErrorMessage = "Description should be at least {2} characters long.")]
+
+        public string Description { get; set; } = null!;
+
+        [Display(Name = "Board")]
+
+        public int BoardId { get; set; }
+
+        public IEnumerable<TaskBoardModel> Boards { get; set; } = null!;
+
+
+
     }
 }
